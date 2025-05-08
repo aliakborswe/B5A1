71afca3945a76b2b057a93ea053d76bfa6e4e4a8
aliakborswe <aliakbor.js@gmail.com>
2025-05-08 12:23:16 +0600

    second problem solved

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@
     }
 
 
+    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+        return items.filter(item => item.rating >= 4);
+    }
+
+
     
 
 }
