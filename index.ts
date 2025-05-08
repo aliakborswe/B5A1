b8f835871c4d0464211f714ab5a00a209114ff95
aliakborswe <aliakbor.js@gmail.com>
2025-05-08 14:29:01 +0600

    seventh problem solved

--- index.ts
+++ index.ts
@@ -70,6 +70,15 @@
         return mostExpensive;
     }
 
-     
+    enum Day {  Monday,  Tuesday,  Wednesday,  Thursday,  Friday,  Saturday,  Sunday}
+
+    function getDayType(day: Day): string {
+        if(day === Day.Saturday || day === Day.Sunday) {
+            return "Weekend";
+        }
+        return "Weekday";
+    }
+
+    
 
 }
