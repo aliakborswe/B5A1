18ce55e6ea233248230f1c06f3ab9bf40082e968
aliakborswe <aliakbor.js@gmail.com>
2025-05-08 12:15:00 +0600

    First problem solved

---
+++ index.ts
@@ -0,0 +1,13 @@
+{
+    function formatString(input: string, toUpper?: boolean): string {
+        if(toUpper === false){
+            return input.toLowerCase();
+        }else {
+            return input.toUpperCase();
+        }
+    }
+
+
+    
+
+}
