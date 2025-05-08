88be54616fdb3f74202a5dea562fa5fa88f483b1
aliakborswe <aliakbor.js@gmail.com>
2025-05-08 14:49:13 +0600

    eighth problem solved

--- index.ts
+++ index.ts
@@ -79,6 +79,16 @@
         return "Weekday";
     }
 
-    
+    async function squareAsync(n: number): Promise<number> {
+        return new Promise((resolve, reject) => {
+            if( n < 0){
+                reject(new Error("Negative number not allowed"));
+            }
+            setTimeout(() => {
+                resolve(n * n);
+            }, 1000);
+        })
+        
+    }
 
 }
