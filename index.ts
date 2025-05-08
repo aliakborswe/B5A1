81a8872727e9c6e059ebee92ff38c6a7f53c1588
aliakborswe <aliakbor.js@gmail.com>
2025-05-08 13:10:37 +0600

    fifth problem solved

--- index.ts
+++ index.ts
@@ -44,6 +44,14 @@
         }
     }
 
+    function processValue(value: string | number): number{
+        if(typeof value === 'string'){
+            return value.length;
+        } else {
+            return value * 2;
+        }
+    }
+
     
 
 }
